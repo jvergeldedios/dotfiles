a4f7a56704aeb7b0cde8dfffbc0aeab3894debf2
Julian Vergel de Dios <julian.vergeldedios@gmail.com>
2015-07-18 14:27:04 -0700

    a few tweaks to brew install process

--- install.sh
+++ install.sh
@@ -13,17 +13,28 @@ then
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
 elif [ $OS_NAME == "Linux" ]
 then
-  sudo apt-get install build-essential curl git m4 ruby texinfo \
-    libbz2-dev libcurl4-openssl-dev libexpat-dev libncurses-dev zlib1g-dev
+  PACKAGES=(build-essential curl git m4 ruby texinfo libbz2-dev libcurl4-openssl-dev libexpat-dev libncurses-dev zlib1g-dev)
+  for package in $PACKAGES do
+    dpkg -s $package
+    if [ $? == 1 ]
+    then
+      sudo apt-get install $package
+    fi
+  done
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/linuxbrew/go/install)";
 else
   echo "This system not compatible with homebrew... exiting."
   exit 1
 fi
 
-# brew update
-# brew upgrade
-# brew doctor
+brew update
+brew upgrade
+brew doctor
+
+if [ $? != 0 ]
+then
+  echo "Fix brew issues then run this script again."
+fi
 
 # install fish
 # brew install fish
