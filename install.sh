9492d721728b76c6d15767fd41ae38e67947ad23
Julian Vergel de Dios <julian.vergeldedios@gmail.com>
2015-07-18 12:48:21 -0700

    adding vim plugin submodules and modifying install script

---
+++ install.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+OS_NAME=`uname`
+RUBY_VERSION=2.2.1
+
+# update submodules
+(cd $DIR && git submodule update --init --recursive)
+
+# install brew
+if [ $OS_NAME == 'Darwin' ]
+then
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
+elif [ $OS_NAME == 'Linux' ]
+then
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/linuxbrew/go/install)";
+else
+  echo "This system not compatible with homebrew... exiting."
+  exit 1
+fi
+
+brew update
+brew upgrade
+brew doctor
+
+# install fish
+brew install fish
+
+# install rbenv
+brew install rbenv
+brew install ruby-build
+
+# install ruby
+rbenv install $RUBY_VERSION
+rbenv rehash
+
+# vim config and plugins
+ln -s $DIR/vimrc $HOME/.vimrc
