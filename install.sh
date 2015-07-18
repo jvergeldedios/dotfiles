1ca3e1fe62139923315e0e7e785f95910455ff4a
Julian Vergel de Dios <julian.vergeldedios@gmail.com>
2015-07-18 12:57:13 -0700

    disabling some installs temporarily

--- install.sh
+++ install.sh
@@ -13,26 +13,28 @@ then
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
 elif [ $OS_NAME == 'Linux' ]
 then
+  sudo apt-get install build-essential curl git m4 ruby texinfo \
+    libbz2-dev libcurl4-openssl-dev libexpat-dev libncurses-dev zlib1g-dev
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/linuxbrew/go/install)";
 else
   echo "This system not compatible with homebrew... exiting."
   exit 1
 fi
 
-brew update
-brew upgrade
-brew doctor
+# brew update
+# brew upgrade
+# brew doctor
 
 # install fish
-brew install fish
+# brew install fish
 
 # install rbenv
-brew install rbenv
-brew install ruby-build
+# brew install rbenv
+# brew install ruby-build
 
 # install ruby
-rbenv install $RUBY_VERSION
-rbenv rehash
+# rbenv install $RUBY_VERSION
+# rbenv rehash
 
 # vim config and plugins
 ln -s $DIR/vimrc $HOME/.vimrc
